0a73b20c72297a9a8ed213f7016d1c9fbb7d0f40
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-05-30 23:00:47 +0530

    C++

--- LeetCodeQues/Q6_ContainDuplicate.cpp
+++ LeetCodeQues/Q6_ContainDuplicate.cpp
@@ -6,17 +6,30 @@ int main(){
     vector<int> nums={1,2,3,1};
     sort(nums.begin(),nums.end());
         int sz=nums.size();
-        bool isDuplicate;
 
-        for(int i=0,j=i+1;i<sz,j<sz;i++,j++){
-             if(nums[i]==nums[j]){
+        bool isDuplicate=false;
+
+        sort(nums.begin(),nums.end());
+
+        // iss loop me c++ i<sz ko ignore kr deta hai to make code effective isliye code duplicate search krne phele hi break hojayge 
+
+        // for(int i=0,j=i+1;i<sz,j<sz;i++,j++){
+        //      if(nums[i]==nums[j]){
+        //         isDuplicate=true;
+        //         break;
+        //     }else{
+        //         isDuplicate=false;
+        //         break;
+        //     }
+        // } 
+
+        for(int i=0;i<sz-1;i++){
+             if(nums[i]==nums[i+1]){
                 isDuplicate=true;
                 break;
-            }else{
-                isDuplicate=false;
-                break;
             }
-        }    
+        }
+
         cout<<isDuplicate<<endl;
 
     return 0;
