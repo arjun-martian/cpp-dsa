fc62ea6512b7008ce1851a8e7f832a0fdb2cbfc0
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-06-03 12:11:20 +0530

    C++

--- LeetCodeQues/Q3_PalindromeNum.cpp
+++ LeetCodeQues/Q3_PalindromeNum.cpp
@@ -3,22 +3,30 @@ using namespace std;
 
 int main(){
     long long num=121;
-    long long rvrs=0;
-    bool isPalindrome=true;
-    int original=num;
+    // long long rvrs=0;
+    // bool isPalindrome=true;
+    // int original=num;
 
-    while(num>0){
-        rvrs*=10;
-        rvrs+=num%10;
-        num/=10;
-    }
+    // while(num>0){
+    //     rvrs*=10;
+    //     rvrs+=num%10;
+    //     num/=10;
+    // }
 
-    if(rvrs==original){
-        cout<<isPalindrome<<endl;
-    }else{
-        isPalindrome=false;
-        cout<<isPalindrome<<endl;
-    }
+    // if(rvrs==original){
+    //     cout<<isPalindrome<<endl;
+    // }else{
+    //     isPalindrome=false;
+    //     cout<<isPalindrome<<endl;
+    // }
+
+    string str = to_string(num);
+        for(int i=0,j=str.length()-1; i<=str.length()/2; i++,j--){
+            if(str[i]!=str[j]) {
+                cout<<"false"<<endl;
+            }
+        }
+    cout<<"true"<<endl;
 
     return 0;
 }
