bebd9a5d7884719e9c8d3c9749282cb273bbf318
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-06-26 08:52:21 +0530

    C++

---
+++ 08_SortingAlgorithm/03_InsertionSort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void insertion_sort(vector<int>& nums,int n);
+void print(vector<int>& nums,int n);
+
+int main(){
+    vector<int> nums={5,4,1,3,2};
+    int n = nums.size();
+
+    insertion_sort(nums,n);
+
+}
+
+void insertion_sort(vector<int>& nums,int n){
+     for(int i=1;i<n;i++){
+        int curr = nums[i];
+        int prvs = i-1;
+          while(prvs>=0 && nums[prvs]>curr){
+            swap(nums[prvs],nums[prvs+1]);
+            prvs--;
+          }
+        nums[prvs+1] = curr;
+     }
+   print(nums,n);
+}
+
+void print(vector<int>& nums,int n){
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<endl;
+    }
+}
