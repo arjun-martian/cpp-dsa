af337bb3d5e4d63473c605b3d5c7557478b4f992
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-06-27 21:30:04 +0530

    C++

---
+++ 08_SortingAlgorithm/04_CountingSort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+void counting_sorting(int num[],int n);
+void print(int num[],int n);
+
+int main(){
+    int num[8]={1,4,1,3,2,4,3,7};
+    int n = sizeof(num)/sizeof(int);
+    
+    counting_sorting(num,n);
+}
+
+void print(int num[],int n){
+    for(int i=0;i<n;i++){
+        cout<<num[i]<<endl;
+    }
+}
+
+void counting_sorting(int num[],int n){
+     int freq[10000]={0}; // range
+     int maxNum = INT_MIN, minNum =INT_MAX;
+
+     for(int i=0;i<n;i++){
+        maxNum = max(num[i],maxNum);
+        minNum = min(num[i],minNum);
+     }
+
+     // 1st step
+     for(int i=0;i<n;i++){
+         freq[num[i]]++;
+     }
+     
+     // 2nd step
+     for(int i=minNum,j=0;i<maxNum;i++){
+        while(freq[i]>0){
+            num[j] = i;
+            freq[i]--;
+            j++;
+        }
+     }
+
+   print(num,n);
+}
