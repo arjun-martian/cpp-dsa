f7d332a88781969141d4d36fb794d79bf733e714
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-08-16 00:00:29 +0530

    C++

--- 10strings.cpp/practice.cpp
+++ 10strings.cpp/practice.cpp
@@ -8,10 +8,18 @@ int main(){
     //     if(!isupper(fruit[i])) fruit[i]-=32;
     //  }
     
+    //CONVERT TO UPPERCASE
+    // for(int i=0;i<strlen(fruit);i++){
+    //     char ch=fruit[i];
+    //     if(ch>='a' && ch<='z'){
+    //         fruit[i]=ch-'a'+'A';
+    //     } 
+    // }
+
     for(int i=0;i<strlen(fruit);i++){
         char ch=fruit[i];
-        if(ch>='a' && ch<='z'){
-            fruit[i]=ch-'a'+'A';
+        if(ch>='A' && ch<='Z'){
+            fruit[i]=ch-'A'+'a';
         } 
     }
     cout<<fruit<<endl;
