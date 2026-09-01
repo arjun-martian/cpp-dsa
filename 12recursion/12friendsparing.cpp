b28338dec59ea45fd818843e21dac0acc0260c7d
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-09-02 01:51:05 +0530

    C++

---
+++ 12recursion/12friendsparing.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int friendspairing(int n){
+     if(n<=2) return n;
+
+     return friendspairing(n-1)+friendspairing(n-1)*friendspairing(n-2);
+
+}
+
+int main(){
+    friendspairing(5);
+
+    return 0;
+}
